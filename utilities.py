1b8728f5f6744272767d3242c2969b42ef04fcb6
Yves Fernandes <ednafr7@gmail.com>
2020-02-26 17:54:52 -0600

    added test data cleaning process

--- utilities.py
+++ utilities.py
@@ -55,7 +55,34 @@ newtype = {
      'ind_recibo_ult1': 'uint8'
 }
 
+new_type_test = {
+     'fecha_dato': 'str',
+     'ncodpers': 'int32',
+     'ind_empleado': 'str',
+     'pais_residencia': 'str',
+     'sexo': 'str',
+     'age': 'str',
+     'fecha_alta': 'str',
+     'ind_nuevo': 'float32',
+     'antiguedad': 'str',
+     'indrel': 'float32',
+     'ult_fec_cli_1t': 'str',
+     'indrel_1mes': 'str',
+     'tiprel_1mes': 'str',
+     'indresi': 'str',
+     'indext': 'str',
+     'conyuemp': 'str',
+     'canal_entrada': 'str',
+     'indfall': 'str',
+     'tipodom': 'float32',
+     'cod_prov': 'float32',
+     'nomprov': 'str',
+     'ind_actividad_cliente': 'str',
+     'renta': 'float32',
+     'segmento': 'str'}
+
 cols = list(newtype.keys())
+test_cols = list(new_type_test.keys())
 chunksize = 1e6
 
 
@@ -69,6 +96,12 @@ def DfLowMemory(filename):
     data = pd.concat(samples)
     
     return data
+
+def DfLowMemoryTest(test_filename):
+    
+    test_data = pd.read_csv(test_filename, usecols=test_cols, dtype=new_type_test)
+
+    return test_data
 
     
 def CleanData(df_data):
@@ -120,13 +153,63 @@ def CleanData(df_data):
     for c in ['segmento']:
         df.loc[df[c].isnull(), c ] = 'UNKOWN'
         
+    df.loc[df['antiguedad'] == '-999999', 'antiguedad'] = 'UNKNOWN'
     df['ind_nomina_ult1'].fillna(df['ind_nomina_ult1'].mode()[0], inplace=True)
     df['ind_nom_pens_ult1'].fillna(df['ind_nom_pens_ult1'].mode()[0], inplace=True)
-    df.loc[df['antiguedad'] == '-999999', 'antiguedad'] = 'UNKNOWN'
-    
+            
     return df
 
+
+def CleanTestData(test):
     
+    df = test[test['ind_empleado'] != 'S']
+    df.dropna(subset=['pais_residencia'], inplace=True)
+    df['sexo'].fillna(df['sexo'].mode()[0], inplace=True)
+    df.loc[df['ult_fec_cli_1t'].isnull(), 'ult_fec_cli_1t'] = 'PRIMARY'
+    map_dict = {'1.0' : '1',
+                '1' : '1',
+                '2' : '2',
+                '2.0' : '2',
+                '3' : '3',
+                '3.0' : '3', 
+                '4' : '4',
+                '4.0' : '4'}
+
+    df.indrel_1mes.fillna('P', inplace=True)
+    df.indrel_1mes = df.indrel_1mes.apply(lambda x: map_dict.get(x,x))
+    df.indrel_1mes = df.indrel_1mes.astype('category')
+
+    df.loc[df['tiprel_1mes'].isnull(), 'tiprel_1mes'] = df['ind_actividad_cliente']
+
+    map_tip = {1 : 'A',
+               0 : 'I'}        
+
+    df.tiprel_1mes = df.tiprel_1mes.apply(lambda x: map_tip.get(x,x))
+    df['tiprel_1mes'].fillna(df['tiprel_1mes'].mode()[0], inplace=True)
+    
+    df.tiprel_1mes = df.tiprel_1mes.astype('category')
+    
+    df['conyuemp'].fillna(df['conyuemp'].mode()[0], inplace=True)
+    df.conyuemp = df.conyuemp.astype('category')
+    
+    df['canal_entrada'].fillna(df['canal_entrada'].mode()[0], inplace=True)
+    
+    df['tipodom'].fillna(df['tipodom'].mode()[0], inplace=True)
+    
+
+    for c in ['cod_prov', 'nomprov']:
+        df.loc[df[c].isnull(), c ] = 'UNKOWN'
+        
+    salaries = dict(df.groupby('nomprov')['renta'].mean().round(0))
+    df.loc[df['renta'].isnull(), 'renta'] = df['nomprov']
+    df.renta = df.renta.apply(lambda x: salaries.get(x,x))
+    
+    for c in ['segmento']:
+        df.loc[df[c].isnull(), c ] = 'UNKOWN'
+        
+    df.loc[df['antiguedad'] == '-999999', 'antiguedad'] = 'UNKNOWN'
+            
+    return df
 
     
 def SampleLowMemory(filename):
